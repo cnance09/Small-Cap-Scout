2b37539bff26ef9dcef06a6229e19cc57ed7290e
Christopher Nance <christophernance@hotmail.com>
2024-09-06 14:25:53 +0200

    Updated data.py file. Module can now return pre-preprocessed dataset.

---
+++ smallscout/params.py
@@ -0,0 +1,30 @@
+import os
+import numpy as np
+
+##################  VARIABLES  ##################
+MODEL_TARGET = os.environ.get("MODEL_TARGET")
+EVALUATION_START_DATE = os.environ.get("EVALUATION_START_DATE")
+GCP_PROJECT = os.environ.get("GCP_PROJECT")
+GCP_PROJECT_WAGON = os.environ.get("GCP_PROJECT_WAGON")
+GCP_REGION = os.environ.get("GCP_REGION")
+BUCKET_NAME = os.environ.get("BUCKET_NAME")
+INSTANCE = os.environ.get("INSTANCE")
+MLFLOW_TRACKING_URI = os.environ.get("MLFLOW_TRACKING_URI")
+MLFLOW_EXPERIMENT = os.environ.get("MLFLOW_EXPERIMENT")
+MLFLOW_MODEL_NAME = os.environ.get("MLFLOW_MODEL_NAME")
+PREFECT_FLOW_NAME = os.environ.get("PREFECT_FLOW_NAME")
+PREFECT_LOG_LEVEL = os.environ.get("PREFECT_LOG_LEVEL")
+GAR_IMAGE = os.environ.get("GAR_IMAGE")
+GAR_MEMORY = os.environ.get("GAR_MEMORY")
+DOCKER_REPO_NAME = os.environ.get("DOCKER_REPO_NAME")
+DOCKER_IMAGE_NAME = os.environ.get("DOCKER_IMAGE_NAME")
+SERVICE_URL = os.environ.get("SERVICE_URL")
+
+# Temporary Paths
+BS_PATH = os.environ.get("BS_PATH")
+IS_PATH = os.environ.get("IS_PATH")
+CF_PATH = os.environ.get("CF_PATH")
+SUB_PATH = os.environ.get("SUB_PATH")
+FRED_PATH = os.environ.get("FRED_PATH")
+MC_PATH = os.environ.get("FRED_PATH")
+STOCK_PATH = os.environ.get("FRED_PATH")
