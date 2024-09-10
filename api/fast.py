2a3fbca0ed8ee0002d44ff35ca4c434aee16b762
Christopher Nance <christophernance@hotmail.com>
2024-09-11 01:14:27 +0200

    Updated API settings

--- api/fast.py
+++ api/fast.py
@@ -1,9 +1,12 @@
 import pandas as pd
 from fastapi import FastAPI, HTTPException
 from smallscout.params import *
+from google.cloud import bigquery
+
 
 #from smallscout.preprocessor import *
 from smallscout.preprocessor import preprocess_new_data
+from smallscout.utils import get_model_file
 from fastapi.middleware.cors import CORSMiddleware
 import pickle
 
@@ -20,26 +23,26 @@ app.add_middleware(
     allow_headers=["*"],  # Allows all headers
 )
 
-# Load the models
 
+# Load the models
 # Load the pre-trained logistic regression model
-with open(MODEL_PATH, 'rb') as f_qrt:
-    app.state.model = pickle.load(f_qrt)
+#with open(MODEL_PATH, 'rb') as f_qrt:
+#    app.state.model = pickle.load(f_qrt)
 
 # Load the preprocessor pipeline
-with open(PREPROCESSOR_PATH, 'rb') as f_preprocessor:
-    app.state.preprocessor = pickle.load(f_preprocessor)
+#with open(PREPROCESSOR_PATH, 'rb') as f_preprocessor:
+#    app.state.preprocessor = pickle.load(f_preprocessor)
 
 # Load the dataset when the app starts
 # Load dataset containing information about all tickers
-try:
-    app.state.dataset = pd.read_csv(QUERY_PATH, index_col=0)
-except Exception as e:
-    raise HTTPException(status_code=500, detail=f"Error loading dataset: {str(e)}")
+# try:
+#     app.state.dataset = pd.read_csv(QUERY_PATH, index_col=0)
+# except Exception as e:
+#     raise HTTPException(status_code=500, detail=f"Error loading dataset: {str(e)}")
 
 
 @app.get("/predict")
-def predict(ticker: str):
+def predict(ticker, model_type='logistic_regression', quarter='2024-Q2', sequence=4, horizon='year-ahead', threshold='50%', small_cap=True):
     """
     Predict the worthiness of a stock based on its ticker symbol.
     Fetches data from the local dataset, preprocesses it, and passes it to the logistic regression model.
@@ -51,19 +54,55 @@ def predict(ticker: str):
         - dict: A JSON object with the prediction result (e.g., worthiness score).
     """
 
+    # Load corresponding model & preprocessor
+    model_file, prep_file = get_model_file(model_type=model_type, sequence=sequence, horizon=horizon, threshold=threshold, small_cap=True)
+    with open(model_file, 'rb') as f_qrt:
+        app.state.model = pickle.load(f_qrt)
+
+    with open(prep_file, 'rb') as f_preprocessor:
+        app.state.preprocessor = pickle.load(f_preprocessor)
+
+    # Load data from Google Big Query
+    query = f"""
+        SELECT *
+        FROM {GCP_PROJECT}.{BQ_DATASET}.{BQ_REGION}
+        WHERE TICKER = '{ticker}'
+        ORDER BY DATE
+        """
+    try:
+        client = bigquery.Client(project=GCP_PROJECT)
+        query_job = client.query(query)
+        result = query_job.result()
+        data = result.to_dataframe()
+    except:
+        if (len(data) == 0)|(data == None):
+            raise HTTPException(status_code=404, detail=f"Ticker {ticker} not found in dataset.")
+        raise ValueError('Error Loading Dataset')
+    print(model_file, prep_file)
+
+
+    if model_type=='RNN':
+        idx = data.index[data.quarter==quarter]
+        input_data = data.iloc[(idx-3):(idx)]
+    else:
+        input_data = data[data.quarter==quarter]
+    input_data.drop(columns=['TICKER', 'date', 'quarter'], inplace=True)
+
+
     # Filter the dataset to get the row for the input ticker
-    ticker_data =  app.state.dataset[app.state.dataset['TICKER'] == ticker]
-    ticker_data['target'] = 0
-    if ticker_data.empty:
-        raise HTTPException(status_code=404, detail=f"Ticker {ticker} not found in dataset.")
+    # ticker_data =  app.state.dataset[app.state.dataset['TICKER'] == ticker]
+    # ticker_data['target'] = 0
+    # if ticker_data.empty:
+    #     raise HTTPException(status_code=404, detail=f"Ticker {ticker} not found in dataset.")
 
     # Preprocess the data for model input
 
     # Preprocess the data using the loaded preprocessor
-    preprocessor = app.state.preprocessor
-    X_processed = preprocess_new_data(ticker_data, preprocessor)
+    # preprocessor = app.state.preprocessor
+    # X_processed = preprocess_new_data(ticker_data, preprocessor)
 
-    print(pd.DataFrame(X_processed).head())
+    X_processed = preprocess_new_data(input_data, app.state.preprocessor)
+
     # Retrieve the preloaded model
     model = app.state.model
     if model is None:
@@ -76,14 +115,15 @@ def predict(ticker: str):
     worthiness = "worthy" if y_pred[0] == 1 else "not worthy"
 
     # Return the prediction result
-    return {"ticker": ticker, "worthiness": worthiness}
+    return {"ticker": ticker, "worthiness": worthiness, 'prediction': y_pred}
 
 @app.get("/")
 def root():
     return {"message": "Welcome to the stock worthiness prediction API!"}
 
-print(app.state.dataset.head())
+#print(app.state.dataset.head())
 
-print(app.state.model == None)
+#print(app.state.model == None)
 
-print(predict("AAPL"))
+print(predict("AAPL", quarter='2023-Q4'))
+# returns --> {'ticker': 'AAPL', 'worthiness': 'not worthy', 'prediction': array([0])}
--- smallscout/utils.py
+++ smallscout/utils.py
@@ -1,4 +1,25 @@
+import pandas as pd
+
 def convert_dates(df):
     df['Year'] = pd.to_datetime(df['Date']).dt.year
     df['Month'] = pd.to_datetime(df['Date']).dt.month
     return df.drop(columns=['Date'])  # Drop the original 'Date' column after conversion
+
+def get_model_file(model_type, sequence=4, horizon='year-ahead', threshold='50%', small_cap=True):
+    if horizon not in ['quarter-ahead', 'year-ahead', 'two-years-ahead']:
+        raise ValueError (f"Unsupported horizon: {horizon}, must be quarter-ahead, year-ahead or two-years-ahead")
+    if threshold not in ['30%', '50%']:
+        raise ValueError (f"Unsupported growth threshold: {threshold}, must be 30% or 50%")
+
+    if model_type in ['logistic_regression', 'knn', 'svc', 'mlpclassifier']:
+        file_name = f"Models/{model_type}_sc{small_cap}_{horizon}_{threshold}.pkl"
+        prep_file = f"Models/preprocessor_cross_section.pkl"
+        return file_name, prep_file
+    if model_type == 'rnn':
+        file_name = f"Models/{model_type}_sc{small_cap}_{horizon}_{sequence}_seq_{threshold}.pkl"
+        prep_file = f"Models/prepocessor_rnn.pkl"
+        return file_name, prep_file
+
+    raise ValueError(f"Unknown model type: {model_type}")
+
+#print(get_model_file('rnn'))
